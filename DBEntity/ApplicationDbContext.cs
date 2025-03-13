4d9a011bd485d80ef64c0794a11423b24f7c17dc
shun-1129 <shun.1129.y@gmail.com>
2025-03-14 00:39:17 +0900

    [Add] ユーザ定義マスタ追加

--- DBEntity/ApplicationDbContext.cs
+++ DBEntity/ApplicationDbContext.cs
@@ -1,9 +1,73 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SampleSystem.DBEntity.Entities.Masters;
 
 namespace SampleSystem.DBEntity
 {
     public class ApplicationDbContext : DbContext
     {
+        #region メンバ変数
+        /// <summary>
+        /// DB接続情報
+        /// </summary>
+        private string _connectInfo = string.Empty;
+        #endregion
 
+        #region コンストラクタ
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
+        public ApplicationDbContext () { }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="connentInfo">DB接続情報</param>
+        public ApplicationDbContext ( string connentInfo ) : this ()
+        {
+            _connectInfo = connentInfo;
+        }
+        #endregion
+
+        #region DBSet
+        /// <summary>
+        /// ユーザ定義マスタ
+        /// </summary>
+        public DbSet<m_user_definition> m_user_definitions { get; set; }
+        #endregion
+
+        #region 内部メソッド
+        /// <summary>
+        /// DB接続設定
+        /// </summary>
+        /// <param name="optionsBuilder">オプションビルダー</param>
+        protected override void OnConfiguring ( DbContextOptionsBuilder optionsBuilder )
+        {
+            // DB接続
+            optionsBuilder.UseNpgsql ( _connectInfo );
+            // 時間設定
+            AppContext.SetSwitch ( "Npgsql.EnableLegacyTimestampBehavior" , true );
+        }
+
+        /// <summary>
+        /// モデル作成
+        /// </summary>
+        /// <param name="modelBuilder">モデルビルダー</param>
+        protected override void OnModelCreating ( ModelBuilder modelBuilder )
+        {
+            #region マスタテーブル
+            // ユーザ定義マスタ
+            modelBuilder.Entity<m_user_definition> ( entity =>
+            {
+                // 主キー定義
+                entity.HasKey ( e => e.id );
+                // 論理削除(falseのみ取得)
+                entity.HasQueryFilter ( e => !e.is_deleted );
+            } );
+            #endregion
+
+            #region トランザクションテーブル
+            #endregion
+        }
+        #endregion
     }
 }
---
+++ DBEntity/Entities/Masters/m_user_definition.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SampleSystem.DBEntity.Entities.Masters
+{
+    [Table ( "m_user_definition" )]
+    public class m_user_definition : ColumnBase
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        [Key]
+        [Required]
+        public int id { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        [Required]
+        public string name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 論理削除
+        /// </summary>
+        [Required]
+        public bool is_deleted { get; set; }
+    }
+}
