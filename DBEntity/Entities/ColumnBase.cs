dab55b3ea7d5d10604bb1246fe979aa3ecc2aad7
shun-1129 <shun.1129.y@gmail.com>
2025-03-14 00:20:25 +0900

    [Add] DBEntity追加（処理未実装）

---
+++ DBEntity/Entities/ColumnBase.cs
@@ -0,0 +1,35 @@
+﻿namespace SampleSystem.DBEntity.Entities
+{
+    public class ColumnBase
+    {
+        /// <summary>
+        /// 作成日時
+        /// </summary>
+        public DateTime create_at { get; set; }
+
+        /// <summary>
+        /// 作成ユーザー
+        /// </summary>
+        public string create_user { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 作成プログラム
+        /// </summary>
+        public string create_program { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 更新日時
+        /// </summary>
+        public DateTime update_at { get; set; }
+
+        /// <summary>
+        /// 更新ユーザー
+        /// </summary>
+        public string update_user { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 更新プログラム
+        /// </summary>
+        public string update_program { get; set; } = string.Empty;
+    }
+}
